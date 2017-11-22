d941785f443c6bbf25a462aecef11031bfbf80c4
Juni Lee <juni.lee@contactenergy.co.nz>
2017-11-23 11:13:06 +1300

    added Services to Startup and appsettings template

--- MeterReadsAPI/Startup.cs
+++ MeterReadsAPI/Startup.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeterReadsAPI.Data;
+using MeterReadsAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +27,14 @@ namespace MeterReadsAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var sqlConnectionString = Configuration.GetConnectionString("DataAccessPostgreSqlProvider");
+
+            services.AddDbContext<MeterReadsDbContext>(options =>
+                options.UseNpgsql(sqlConnectionString)
+            );
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddSingleton<IMeterReadsRepository, MeterReadsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
